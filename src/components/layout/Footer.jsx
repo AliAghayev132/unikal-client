6fcfd4e758575951493ae0b6656c3e61587be30e
mouradbalayev <muradbalazade@gmail.com>
2025-09-06 15:10:17 +0400

    site fixed

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -155,7 +155,7 @@ const Footer = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span className="text-white">Niyazi küçəsi, 121, Sumqayıt şəhəri, AZ5009</span>
+                <span className="text-white">Sumqayıt şəhəri, İsmət Qayıbov küçəsi, giriş 2, AZ5009</span>
               </li>
               <li className="flex items-start space-x-3">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-400 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
